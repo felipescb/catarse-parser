8815aaaaf31c10b8e4e91846cedcf7389143adfb
Felipe <de.souza258@gmail.com>
2019-08-21 23:23:15 -0300

    Catarse Parsing Beta 1

---
+++ interpreter.js
@@ -0,0 +1,45 @@
+const HTMLParser = require('node-html-parser');
+const fs = require('fs');
+const path = process.cwd();
+const buffer = fs.readFileSync(path + "\\cards2.html");
+const csvjson = require('csvjson');		
+
+const data = buffer.toString();
+const root = HTMLParser.parse(data);
+
+//console.log(root.querySelectorAll(".w-row"));
+
+let cards = root.querySelectorAll(".card");  
+let session = [];
+
+cards.forEach((el, index, array) => {
+	let cash = (el.querySelector('.card-project-stats .u-text-center-small-only .fontweight-semibold')||{}).innerHTML || "";
+	let title = (el.querySelector('.link-hidden')||{}).innerHTML || "";
+	let time = (el.querySelector('.card-project-stats .u-text-right .lineheight-tight')||el.querySelector('.card-project-stats .u-text-right .fontweight-semibold')||{}).innerHTML || "";
+	let percentage = (el.querySelector('.card-project-stats .fontsize-base')||{}).innerHTML || "";
+
+	let template = {
+		'title' : title,
+		'percentage' : percentage,
+		'cash' : cash,
+		'time' : time
+	}
+
+	session.push(template);
+
+	if(index === array.length - 1) { 
+		
+		const csvData = csvjson.toCSV(session, {
+			headers: 'key'
+		});
+
+		fs.writeFile('./data.csv', csvData, (err) => {
+			if(err) {
+            	console.log(err); // Do something to handle the error or just throw it
+            	throw new Error(err);
+        	}
+        	console.log('Success!');
+    	});
+
+	}
+});
---
+++ parser.js
@@ -0,0 +1,47 @@
+const Nightmare = require('nightmare');      
+const nightmare = Nightmare({ show: false, waitTimeout: 5000 });                                                                                                                                                                
+const vo = require('vo');     
+
+const loadMore = 'a[href="#loadMore"]';
+const url = 'https://www.catarse.me/explore?ref=ctrse_header'
+
+vo(run)(function(err, result) {
+  if (err) throw err;
+});
+
+function* run() {
+  let MAX_PAGE = 30;
+  let currentPage = 0;
+  let nextExists = true;
+  let cards = [];
+
+  yield nightmare
+    .goto(url)
+    .wait(loadMore)
+    .click(loadMore)
+    .wait(loadMore)
+
+  nextExists = yield nightmare.visible(loadMore);
+
+  while (nextExists && currentPage < MAX_PAGE) {
+    cards.push(yield nightmare
+      .evaluate(function() {
+        return [].slice.call(document.querySelectorAll(".card")).map((c) => c.outerHTML);
+      }));
+
+    yield nightmare
+      .click(loadMore)
+      .wait(loadMore)
+
+    console.log(currentPage++);
+    nextExists = yield nightmare.visible(loadMore);
+  }
+
+  let fs = require('fs');
+  fs.writeFile ("cards2.html", cards, function(err) {
+    if (err) throw err;
+    console.log('complete');
+  });
+
+  yield nightmare.end();
+}
